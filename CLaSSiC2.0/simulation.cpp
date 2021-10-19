c26581f308cfeaab8e60753f12499f5e8b6848a2
timooo0@hotmail.com <36dAF5VchEsS>
2021-10-19 16:18:47 +0200

    Changed delete data files on run Added rotor mode for init

--- CLaSSiC2.0/simulation.cpp
+++ CLaSSiC2.0/simulation.cpp
@@ -4,18 +4,15 @@
 #include <cmath>
 #include <random>
 #include <algorithm>
+#include <filesystem>
 #include <chrono>
 
 Simulation::Simulation()
 {
 	int i = 0;
 	int succes = 0;
-	while (succes == 0) {
-		std::string dataFile = constants::outputFile;
-		dataFile.insert(dataFile.find("."), std::to_string(i));
-		succes = std::remove(constants::outputFile.c_str());
-		i++;
-	}
+	std::filesystem::remove_all("data/");
+	std::filesystem::create_directory("data");
 
 	load();
 	initialize();
@@ -121,6 +118,7 @@ void Simulation::load()
 
 void Simulation::initialize(double angle)
 {
+	float mode = 4;
 	switch (constants::spinInit) {
 		case 0:
 			for (int i = 0; i < constants::nAtoms; i++) {
@@ -139,17 +137,26 @@ void Simulation::initialize(double angle)
 			break;
 		case 2:
 			for (int i = 0; i < constants::nAtoms; i++) {
-				spin[3 * i]     = 0.001*std::cos((float)i / constants::nAtoms * 2 * constants::pi)*std::sin(angle);
-				spin[3 * i + 1] = 0.001*std::sin((float)i / constants::nAtoms * 2 * constants::pi)*std::sin(angle);
-				spin[3 * i + 2] = std::cos(angle);
+				spin[3 * i]     = 0.001*std::cos((float)i / constants::nAtoms * mode * 2 * constants::pi)*std::sin(angle);
+				spin[3 * i + 1] = 0.001*std::sin((float)i / constants::nAtoms * mode *  2 * constants::pi)*std::sin(angle);
+				spin[3 * i + 2] = 1;
 			}
+			// normalize();
 			break;
+		case 3:
+			spin[0] = std::cos(0)*std::sin(angle);
+			spin[1] = 0;
+			spin[2] = std::cos(angle);
+			spin[3] = -spin[0];
+			spin[4] = 0;
+			spin[5] = spin[2];
+		break;
 	}
 
-	//std::cout << "Spin initialization: \n";
-	//for (int i = 0; i < constants::nAtoms; i++) {
-	//	std::cout << "x: " << spin[3 * i] << " y: " << spin[3 * i + 1] << " z: " << spin[3 * i + 2] << std::endl;
-	//}
+	// std::cout << "Spin initialization: \n";
+	// for (int i = 0; i < constants::nAtoms; i++) {
+	// 	std::cout << "x: " << spin[3 * i] << " y: " << spin[3 * i + 1] << " z: " << spin[3 * i + 2] << std::endl;
+	// }
 }
 
 void Simulation::normalize()
@@ -184,9 +191,12 @@ void Simulation::run(int iterator, double temperature)
 	std::string dataFile = constants::outputFile;
 	dataFile.insert(dataFile.find("."),std::to_string(iterator));
 	file.open(dataFile, std::ios::binary);
-
+	double saveAtoms = constants::nAtoms;
+	double saveSteps = constants::steps;
 	file.write((char*)&constants::offset, sizeof(double));
+	file.write((char*)&saveAtoms, sizeof(double));
 	file.write((char*)&constants::dt, sizeof(double));
+	file.write((char*)&saveSteps, sizeof(double));
 	file.write((char*)&constants::J, sizeof(double));
 	file.write((char*)&constants::lambda, sizeof(double));
 	file.write((char*)&constants::magneticField[0], 3*sizeof(double));
@@ -194,6 +204,7 @@ void Simulation::run(int iterator, double temperature)
 	file.write((char*)&temperature, sizeof(double));
 	file.write((char*)&constants::length, sizeof(double));
 
+	double z = spin[2];
 	for (int i = 0; i < constants::steps; i++) {
 		if (constants::temperatureSigma != 0 && temperature != 0) {
 			for (int i = 0; i < constants::nAtoms * 3; i++) {
@@ -201,16 +212,19 @@ void Simulation::run(int iterator, double temperature)
 			}
 		}
 
-		integrator.integrate(neighbours, spin, randomField);
 		if (i % 1000 == 0) {
 			std::cout << "Progress: " << (float)i / constants::steps*100 << "%\r";
 		}
-		normalize();
-		//std::cout << "x: " << spin[0] << " y: " << spin[1] << " z: " << spin[2] << std::endl;
+		integrator.integrate(neighbours, spin, randomField);
+		// normalize();
+
+		// if (i%100==0){
+		// 	file.write((char*)&spin[0], sizeof(double)*constants::nAtoms*3);
+		// }
 		file.write((char*)&spin[0], sizeof(double)*constants::nAtoms*3);
 	}
 	file.close();
-
+	// std::cout << std::endl << z - spin[2] <<std::endl;
 	auto msInt = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
 	std::cout << "\nDuration: " << (float)msInt.count() / 1000 << "seconds" << std::endl;
 }
