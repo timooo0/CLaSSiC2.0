b25be9cce25db6d51ef59e5903b925e6bdc27f5e
timooo0@hotmail.com <36dAF5VchEsS>
2021-10-19 16:16:56 +0200

    moved dt inside rungeKutta

--- CLaSSiC2.0/integrator.cpp
+++ CLaSSiC2.0/integrator.cpp
@@ -1,4 +1,5 @@
 #include "integrator.hpp"
+#include <iostream>
 
 void Integrator::calculateEffectiveField(std::vector<std::vector<double>>& neighbours, std::vector<double>& spin)
 {
@@ -26,35 +27,44 @@ void Integrator::rungeKutta(std::vector<std::vector<double>>& neighbours, std::v
 		pDotp = dotProduct(&spin[3 * i], &spin[3 * i]);
 		pDotB = dotProduct(&spin[3 * i], &effectiveField[3 * i]);
 
-		rk[3 * i] = constants::gamma * (spin[3 * i + 1] * effectiveField[3 * i + 2] - spin[3 * i + 2] * effectiveField[3 * i + 1]
+		rk[3 * i] = constants::gamma * constants::dt * (spin[3 * i + 1] * effectiveField[3 * i + 2] - spin[3 * i + 2] * effectiveField[3 * i + 1]
 			// + constants::lambda*(effectiveField[3*i]*pDotp - spin[3*i]*pDotB));
 			+ constants::lambda*(
 				effectiveField[3*i+1] * spin[3*i] * spin[3*i+1]
 				- effectiveField[3*i] * spin[3*i+1] * spin[3*i+1]
 				+ effectiveField[3*i+2] * spin[3*i] * spin[3*i+2]
 				- effectiveField[3*i] * spin[3*i+2] * spin[3*i+2]));
-		rk[3*i + 1] = constants::gamma * (spin[3 * i + 2] * effectiveField[3 * i] - spin[3 * i] * effectiveField[3 * i + 2]
+		rk[3*i + 1] = constants::gamma * constants::dt * (spin[3 * i + 2] * effectiveField[3 * i] - spin[3 * i] * effectiveField[3 * i + 2]
 			// + constants::lambda * (effectiveField[3 * i + 1] * pDotp - spin[3 * i + 1] * pDotB));
 			+constants::lambda*(
 				- effectiveField[3*i+1] * spin[3*i]*spin[3*i]
 				+ effectiveField[3*i] * spin[3*i] * spin[3*i+1]
 				+ effectiveField[3*i+2] * spin[3*i+1] * spin[3*i+2]
 				- effectiveField[3*i+1] * spin[3*i+2]*spin[3*i+2]));
-		rk[3 * i + 2] = constants::gamma * (spin[3 * i] * effectiveField[3 * i + 1] - spin[3 * i + 1] * effectiveField[3 * i]
+		rk[3 * i + 2] =  constants::gamma * constants::dt *(spin[3 * i] * effectiveField[3 * i + 1] - spin[3 * i + 1] * effectiveField[3 * i]
 			// + constants::lambda * (effectiveField[3 * i + 2] * pDotp - spin[3 * i + 2] * pDotB));
 			+constants::lambda*(
 				- effectiveField[3*i+2] * spin[3*i]*spin[3*i]
 				- effectiveField[3*i+2] * spin[3*i+1]*spin[3*i+1]
 				+ effectiveField[3*i] * spin[3*i]*spin[3*i+2]
 				+ effectiveField[3*i+1] * spin[3*i+1]*spin[3*i+2]));
+
+		// if (rk[3*i+2]!=0){
+		// 	std::cout.precision(std::numeric_limits<double>::digits10+2);
+		// 	std::cout << "Unequal to zero: \n";
+		// 	std::cout << spin[3 * i] * effectiveField[3 * i + 1] << std::endl;
+		// 	std::cout << spin[3 * i + 1] * effectiveField[3 * i] << std::endl;
+		// 	std::cout << spin[3 * i] * effectiveField[3 * i + 1] - spin[3 * i + 1] * effectiveField[3 * i] << std::endl;
+		// }
 	}
 }
 
 void Integrator::integrate(std::vector<std::vector<double>>& neighbours, std::vector<double>& spin, std::vector<double>& randomField)
 {
 	rungeKutta(neighbours, spin);
+	rkSave = rk;
 	for (int i = 0; i < constants::nAtoms * 3; i++) {
-		rkPos[i] = spin[i] + rk[i] * constants::dt / 2;
+		rkPos[i] = spin[i] + rk[i]/2;
 	}
 	rungeKutta(neighbours, rkPos);
 
@@ -64,9 +74,12 @@ void Integrator::integrate(std::vector<std::vector<double>>& neighbours, std::ve
 		temperatureSpin[1] = constants::gamma * (spin[3 * i + 2] * randomField[3 * i] - spin[3 * i] * randomField[3 * i + 2]);
 		temperatureSpin[2] = constants::gamma * (spin[3 * i] * randomField[3 * i + 1] - spin[3 * i + 1] * randomField[3 * i]);
 
-		spin[3*i] += rk[3*i] * constants::dt + temperatureSpin[0];
-		spin[3 * i+1] += rk[3*i+1] * constants::dt + temperatureSpin[1];
-		spin[3 * i+2] += rk[3*i+2] * constants::dt + temperatureSpin[2];
+		// spin[3*i] += 0.5 * rk[3*i] * constants::dt + 0.5 * rkSave[3*i + 0]*constants::dt+ temperatureSpin[0];
+		// spin[3 * i+1] +=  0.5 * rk[3*i+1] * constants::dt + 0.5 * rkSave[3*i + 1]*constants::dt+ temperatureSpin[1];
+		// spin[3 * i+2] +=  0.5 * rk[3*i+2] * constants::dt + 0.5 * rkSave[3*i + 2]*constants::dt+ temperatureSpin[2];
+		spin[3*i] 	  += rk[3*i]   + temperatureSpin[0];
+		spin[3 * i+1] += rk[3*i+1] + temperatureSpin[1];
+		spin[3 * i+2] += rk[3*i+2] + temperatureSpin[2];
 	}
 }
 
