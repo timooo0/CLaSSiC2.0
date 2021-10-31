e73fd3d670c0e149c77cbe9332e5f9aef7c60745
timooo0@hotmail.com <36dAF5VchEsS>
2021-10-31 02:42:29 +0200

    Clean up of the plot

--- Plotting/Zeeman.py
+++ Plotting/Zeeman.py
@@ -7,8 +7,8 @@ fNum  = 0
 atom = 0
 
 print(x.shape)
-fig, ax = plt.subplots(1,3)
-time = np.linspace(0,param[fNum]["steps"]*param[fNum]["dt"],param[fNum]["steps"])
+fig, ax = plt.subplots(1,3,constrained_layout=True)
+time = np.linspace(0,param[fNum]["steps"]*param[fNum]["dt"]*1e12,param[fNum]["steps"])
 #energy = data[3::N]
 
 
@@ -18,24 +18,27 @@ precession = helper.constants["Hz_to_meV"]*np.fft.fftfreq(param[fNum]["steps"],d
 theory = helper.constants["Hz_to_meV"]*helper.constants["gamma"]*param[fNum]["magneticField"][-1]/(2*np.pi)
 print(f'precession energy: {precession} meV, theoretical: {theory} meV, error: {theory/precession}')
 
-end = int(1e5)
-ax[0].plot(time[:end], x[fNum, atom, :end],'r')
-ax[0].plot(time[:end], y[fNum, atom, :end],'b')
-# ax[0].plot(time[:end], z[fNum, atom, :end],'g')
-ax[0].set_xlabel('time [s]')
+end = int(2e5)
+ax[0].plot(time[:end], x[fNum, atom, :end],'r', label='x')
+ax[0].plot(time[:end], y[fNum, atom, :end],'b', label='y')
+ax[0].plot(time[:end], z[fNum, atom, :end],'g', label='z')
+ax[0].legend(loc='upper right')
+ax[0].set_xlabel('Time [ps]')
 ax[0].set_ylabel('spin [-]')
 ax[0].set_title('Spin evolution')
 
-ax[1].plot(helper.constants["Hz_to_meV"]*np.fft.fftfreq(param[fNum]["steps"],d=param[fNum]["dt"]), np.fft.fft(x[fNum, atom, :]).real)
+
+ax[1].plot(helper.constants["Hz_to_meV"]*np.fft.fftfreq(param[fNum]["steps"],d=param[fNum]["dt"]), np.fft.fft(x[fNum, atom, :]).real/np.max(np.fft.fft(x[fNum, atom, :]).real))
 ax[1].set_xlim(-2, 2)
 ax[1].set_xlabel('Energy [meV]')
-ax[1].set_ylabel('relative count [-]')
+ax[1].set_ylabel('relative counts [-]')
 ax[1].set_title('Fourier transform')
 
 
 ax[2].plot(x[fNum, atom, :end], y[fNum, atom, :end],'r')
 ax[2].set_xlabel('x')
-ax[2].set_xlabel('y')
+ax[2].set_ylabel('y')
 ax[2].set_title('x-y plane')
+
 # # plt.axis('equal')
 plt.show()
