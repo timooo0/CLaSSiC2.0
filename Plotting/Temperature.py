94a086c715f7575478fd2f7f5d66437f14bbaa66
timooo0@hotmail.com <36dAF5VchEsS>
2021-10-31 02:42:54 +0200

    Uses helper function now

--- Plotting/Temperature.py
+++ Plotting/Temperature.py
@@ -1,35 +1,14 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-basePath = os.getcwd()+"\\CLaSSiC2.0\\data\\data.dat"
-i = 0
-path = basePath[:-4] + str(i) + basePath[-4:]
+import helper
 
-
-succes = True
+param, x, y, z = helper.getData()
 temperatureArray = []
-spinZ = []
-
-while(succes):
-    try:
-        with open(path, "rb") as f:
-            data = np.fromfile(f, dtype=np.double)
-
-        offset = int(data[0])
-        temperatureArray.append(data[10])
-        length = int(data[11])
-        magneticField = data[4:7]
-        z = data[offset + 2::length]
-        spinZ.append(np.mean(z))
-
-        i+=1
-        path = basePath[:-4] + str(i) + basePath[-4:]
-        print(f'file {i}')
-    except IOError:
-        if i==0:
-            print('Error While Opening the file!')
-        else:
-            succes = False
+zArray = []
+for i in range(x.shape[0]):
+    temperatureArray.append(param[i]["temperature"])
+    zArray.append(np.mean(z[i,0,int(param[i]["steps"]/2):]))
 
 #
 
@@ -37,18 +16,17 @@ while(succes):
 print('plotting...')
 fig, ax = plt.subplots()
 
-y = 2.002 * 7/2 * magneticField[-1]*9.274009994e-24/1.38064852e-23
+y = 2.002 * 7/2 * param[0]["magneticField"][-1]*9.274009994e-24/1.38064852e-23
 
-temperature = np.linspace(1,100,50)
+temperature = np.linspace(1,param[-1]["temperature"],50)
 y = y/np.array(temperature)
 langevin = np.cosh(y)/np.sinh(y) - 1/y
-print(f'Magnetic field: {magneticField}')
+print(f'Magnetic field: {param[0]["magneticField"][-1]}')
 
 ax.plot(temperature, langevin,'b')
-ax.plot(temperatureArray, np.abs(spinZ), 'r.')
+ax.plot(temperatureArray, zArray, 'r.')
 ax.set_xlabel('$T [K]$')
 ax.set_ylabel('$S [-]$')
 #
 # # plt.axis('equal')
 plt.show()
-f.close()
