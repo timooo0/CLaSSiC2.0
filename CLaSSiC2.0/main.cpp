9ae7567b6a37092b5afea4610f021b3be5e2ffa3
timooo0@hotmail.com <36dAF5VchEsS>
2021-11-18 13:21:48 +0100

    Line structure added Set derivates implemented

--- CLaSSiC2.0/main.cpp
+++ CLaSSiC2.0/main.cpp
@@ -28,6 +28,7 @@ int main(int argc, char *argv[])
 		}
 		else if (std::string(argv[i]) == "-anisotropy"){
 			constants::anisotropyStrength = std::stod(argv[i+1]);
+			std::cout << "anisotropyStrength: " << constants::anisotropyStrength << std::endl;
 		}
 		else if (std::string(argv[i]) == "-T"){
 			constants::temperature = std::stod(argv[i+1]);
@@ -37,24 +38,33 @@ int main(int argc, char *argv[])
 		}
 		else if (std::string(argv[i]) == "-angle"){
 			constants::angle = std::stod(argv[i+1])*constants::pi/180.;
+			std::cout << "angle: " << constants::angle << std::endl;
+		}
+		else if (std::string(argv[i]) == "-mode"){
+			constants::mode = std::stoi(argv[i+1]);
 		}
 		else if (std::string(argv[i]) == "-structure"){
 			if (std::string(argv[i+1]) == "single"){
-				constants::nUnitCells = 1;
 				constants::nDimensions = 1;
 			}
+			if (std::string(argv[i+1]) == "line"){
+				constants::nDimensions = 1;
+				constants::unitVectors = {{1., 0., 0.}, {0., 0., 0.}, {0., 0., 0.}};
+			}
 			else if (std::string(argv[i+1]) == "square") {
-				constants::nUnitCells = 1;
 				constants::nDimensions = 2;
 				constants::unitVectors = {{1., 0., 0.}, {0., 1., 0.}, {0., 0., 0.}};
 			} else {
 				std::cerr << "-structure only has the following options: single and square\n";
 			}
 		}
+		else if (std::string(argv[i]) == "-nCellsX"){
+			constants::nUnitCells = (int)std::stod(argv[i+1]);
+		}
 
 		i++;
 	}
-	
+	setDerivatives();
 	Simulation sim;
 	sim.run();
 	
