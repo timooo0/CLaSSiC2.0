4068ead5a609a30032c5e60fe63817866aef622d
timooo0@hotmail.com <36dAF5VchEsS>
2021-11-18 13:19:42 +0100

    Now updates dependending variables

--- CLaSSiC2.0/constants.cpp
+++ CLaSSiC2.0/constants.cpp
@@ -16,46 +16,39 @@ namespace constants
 	// System constants
 	double spinSize = 3.5;
 	int baseAtoms = 1;
-	int nUnitCells = 1;
+	int nUnitCells = 12;
 	int nDimensions = 1;
-	int nAtoms = baseAtoms * std::pow(nUnitCells, nDimensions);
-	std::vector<std::vector<double>> unitVectors = {{(double)baseAtoms, 0., 0.}, {0., 1., 0.}, {0., 0., 0.}};
+	int nAtoms;
+	std::vector<std::vector<double>> unitVectors;
 
 	// Simulation settings;
-	int spinInit = 1; // 0: Z-direction, 1: angle with z-axis, 2: small angle with z-axis, 3: rotor, 4: Z-direction alternating
+	int spinInit = 2; // 0: Z-direction, 1: angle with z-axis, 2: small angle with z-axis, 3: rotor, 4: Z-direction alternating
 	double angle = 0.25 * constants::pi;
+	int mode = 0;
 	double minDistance = 1.1;
-	int nSims = 1;
 
 	// Simulation parameters
-	double dt = 1e-14;
+	double dt = 1e-15;
 	int steps = 1e6;
 	double anisotropy[3] = {0, 0, 1}; // Should be normalized
 	double anisotropyStrength = 0;
-	double magneticField[3] = {0, 0, 50};
-	double J = 0 * boltzmann;
-	double temperature = 0;
-	double lambda = 1e-2;
+	double anisotropyAxis = 0;
+	double anisotropyPlane = 0;
+	double magneticField[3] = {0, 0, 20};
+	double J = 2 * boltzmann;
+	double temperature = 1;
+	double lambda = 1e-3;
 
 	// File constants
 	std::string inputFile = "spin.csv";
 	std::string outputFile = "data/data.dat";
-	double offset = 14; // offset, nAtoms, dt, steps. J, lambda, 3 magneticField, 3 anisotropy, temperature, length
-	double length = 3 * nAtoms;
+	double offset = 15; // offset, nAtoms, dt, steps. J, lambda, 3 magneticField, 3 anisotropy, temperature, length, anisotropyStrength
+	double length;
 
 	// Prefactors
-	double exchangePrefactor = -2 * J * spinSize / (gFactor * bohrMagneton);
-	double temperatureSigma = 2. * lambda * boltzmann * hBar * dt / (gFactor * gFactor * bohrMagneton * bohrMagneton * spinSize);
-	double anisotropyMatrix[9] = {
-		anisotropyStrength * anisotropy[0] * anisotropy[0],
-		anisotropyStrength * anisotropy[0] * anisotropy[1],
-		anisotropyStrength * anisotropy[0] * anisotropy[2],
-		anisotropyStrength * anisotropy[1] * anisotropy[0],
-		anisotropyStrength * anisotropy[1] * anisotropy[1],
-		anisotropyStrength * anisotropy[1] * anisotropy[2],
-		anisotropyStrength * anisotropy[2] * anisotropy[0],
-		anisotropyStrength * anisotropy[2] * anisotropy[1],
-		anisotropyStrength * anisotropy[2] * anisotropy[2]};
+	double exchangePrefactor;
+	double temperatureSigma;
+	double anisotropyMatrix[9];
 
 }
 
@@ -63,3 +56,25 @@ namespace constants
 bool printPositions = false;
 bool printNeighbours = false;
 bool printInitialize = false;
+
+void setDerivatives(){
+	constants::nAtoms = constants::baseAtoms * std::pow(constants::nUnitCells, constants::nDimensions);
+	constants::unitVectors = {{(double)constants::baseAtoms, 0., 0.}, {0., 1., 0.}, {0., 0., 0.}};
+
+	constants::length = 3 * constants::nAtoms;
+
+	constants::exchangePrefactor = -2 * constants::J * constants::spinSize / (constants::gFactor * constants::bohrMagneton);
+	constants::temperatureSigma = 2. * constants::lambda * constants::boltzmann * constants::hBar * constants::dt / 
+		(constants::gFactor * constants::gFactor * constants::bohrMagneton * constants::bohrMagneton * constants::spinSize);
+	double anisM[9] = {
+		constants::anisotropyStrength * constants::anisotropy[0] * constants::anisotropy[0],
+		constants::anisotropyStrength * constants::anisotropy[0] * constants::anisotropy[1],
+		constants::anisotropyStrength * constants::anisotropy[0] * constants::anisotropy[2],
+		constants::anisotropyStrength * constants::anisotropy[1] * constants::anisotropy[0],
+		constants::anisotropyStrength * constants::anisotropy[1] * constants::anisotropy[1],
+		constants::anisotropyStrength * constants::anisotropy[1] * constants::anisotropy[2],
+		constants::anisotropyStrength * constants::anisotropy[2] * constants::anisotropy[0],
+		constants::anisotropyStrength * constants::anisotropy[2] * constants::anisotropy[1],
+		constants::anisotropyStrength * constants::anisotropy[2] * constants::anisotropy[2]};
+	memcpy(constants::anisotropyMatrix, anisM, sizeof(anisM));
+}
