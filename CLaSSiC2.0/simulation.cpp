a83a9085e30948af2ee8a17ba7e42b8704e710f2
timooo0@hotmail.com <36dAF5VchEsS>
2021-11-18 13:22:36 +0100

    Better spin init for antiferromagnet

--- CLaSSiC2.0/simulation.cpp
+++ CLaSSiC2.0/simulation.cpp
@@ -29,11 +29,10 @@ void Simulation::load()
 		4. Apply periodic boundary conditions
 	*/
 
-	//Create the basis atom
-	position.push_back(0); //x
-	position.push_back(0); //y
-	position.push_back(0); //z
-
+	// Create the basis atom
+	position.push_back(0); // x
+	position.push_back(0); // y
+	position.push_back(0); // z
 
 	// Add more unit cells
 	std::vector<double> basePosition;
@@ -93,7 +92,7 @@ void Simulation::load()
 						}
 						else
 						{
-							if (position[3 * i + k] != position[3 * j + k]) 
+							if (position[3 * i + k] != position[3 * j + k])
 							{
 								applyBoundry = false;
 							}
@@ -110,16 +109,15 @@ void Simulation::load()
 		}
 	}
 
-// Printing
-#if printPositions
+	// Printing
+
 	std::cout << "Atom positions: \n";
 	for (int i = 0; i < position.size() / 3; i++)
 	{
 		std::cout << "x: " << position[3 * i] << " y: " << position[3 * i + 1] << " z: " << position[3 * i + 2] << std::endl;
 	}
-#endif
 
-#if printNeighbours
+
 	std::cout << "Neighbours: \n";
 	for (int i = 0; i < constants::nAtoms; i++)
 	{
@@ -129,7 +127,7 @@ void Simulation::load()
 		}
 		std::cout << std::endl;
 	}
-#endif
+
 }
 
 void Simulation::initialize()
@@ -137,11 +135,13 @@ void Simulation::initialize()
 	/*
 	Initializing the spin orientations
 	*/
-	float mode = 4;
+
+	int swap = 1;
+	double delta = 1;
 	switch (constants::spinInit)
 	{
 	case 0:
-		//Alligned with the z-axis
+		// Alligned with the z-axis
 		for (int i = 0; i < constants::nAtoms; i++)
 		{
 			spin[3 * i] = 0;
@@ -150,7 +150,7 @@ void Simulation::initialize()
 		}
 		break;
 	case 1:
-		//Angle with z-axis
+		// Angle with z-axis
 		for (int i = 0; i < constants::nAtoms; i++)
 		{
 			spin[3 * i] = std::cos((float)i / constants::nAtoms * 2 * constants::pi) * std::sin(constants::angle);
@@ -159,43 +159,77 @@ void Simulation::initialize()
 		}
 		break;
 	case 2:
-		//Small z angle for spin waves
-		for (int i = 0; i < constants::nAtoms; i++)
+		// Small z angle for spin waves
+		for (int i = 0; i < (int)constants::nAtoms; i++)
 		{
-			spin[3 * i] = 0.001 * std::cos((float)i / constants::nAtoms * mode * 2 * constants::pi) * std::sin(constants::angle);
-			spin[3 * i + 1] = 0.001 * std::sin((float)i / constants::nAtoms * mode * 2 * constants::pi) * std::sin(constants::angle);
-			spin[3 * i + 2] = 1;
+			if (constants::J < 0)
+				{
+					if (i % 2 == 0)
+					{
+						swap = 1;
+					}
+					else
+					{
+						swap = -1;
+					}
+				}
+				spin[3 * i] = swap * 0.001 * std::cos((float)i /constants::nAtoms * constants::mode * 2 * constants::pi);
+				spin[3 * i + 1] = swap * 0.001 * std::sin((float)i /constants::nAtoms * constants::mode * 2 * constants::pi);
+				spin[3 * i + 2] = swap * 1;
 		}
 		normalize();
 		break;
 	case 3:
-		//Rotor mode for two spins
-		spin[0] = std::cos(0) * std::sin(constants::angle);
-		spin[1] = 0;
+		if (constants::J < 0)
+		{
+			swap = -1;
+		}
+		// Rotor mode for two spins
+		spin[0] = std::cos(constants::angle);
+		spin[1] = std::sin(constants::angle);
 		spin[2] = std::cos(constants::angle);
-		spin[3] = -spin[0];
-		spin[4] = 0;
-		spin[5] = spin[2];
+		spin[3] = swap * std::cos(constants::angle);
+		spin[4] = std::sin(constants::angle); 
+		spin[5] = swap * std::cos(constants::angle);
 		break;
 	case 4:
-		for (int i = 0; i < constants::nAtoms; i++){
+		// 2D spin waves
+		for (int i = 0; i < constants::nAtoms; i++)
+		{
 			spin[3 * i] = 0;
 			spin[3 * i + 1] = 0;
-			if ((i+i/constants::nUnitCells)%2==0){
+			if ((i + i / constants::nUnitCells) % 2 == 0)
+			{
 				spin[3 * i + 2] = 1;
-			} else {
-			spin[3 * i + 2] = -1;
+			}
+			else
+			{
+				spin[3 * i + 2] = -1;
 			}
 		}
-	}
+	case 5:
+		// Triangle
+		std::cout << "Triangle:\n";
+		spin[0] = delta*std::cos(30./180.*constants::pi);
+		spin[1] = delta*std::sin(30./180.*constants::pi);
+		spin[2] = 1;
 
-//Printing
-# if printInitialize
-	std::cout << "Spin initialization: \n";
-	for (int i = 0; i < constants::nAtoms; i++) {
-		std::cout << "Atom: " << i/constants::nUnitCells << ", "<<i%constants::nUnitCells << "|x: " << spin[3 * i] << " y: " << spin[3 * i + 1] << " z: " << spin[3 * i + 2] << std::endl;
+		spin[3] = -delta*std::cos(30./180.*constants::pi);
+		spin[4] = delta*std::sin(30./180.*constants::pi);
+		spin[5] = 1;
+		
+		spin[6] = delta*0;
+		spin[7] = -delta*1;
+		spin[8] = -1;
+	break;
 	}
-# endif
+	// Printing
+
+	std::cout << "Spin initialization: \n";
+	for (int i = 0; i < constants::nAtoms; i++)
+	{
+		std::cout << "Atom: " << i / constants::nUnitCells << ", " << i % constants::nUnitCells << "|x: " << spin[3 * i] << " y: " << spin[3 * i + 1] << " z: " << spin[3 * i + 2] << std::endl;
+	}
 }
 
 void Simulation::run()
@@ -210,11 +244,12 @@ void Simulation::run()
 	*/
 
 	// Open and write to file
-	int i=0;
+	int i = 0;
 	std::ofstream file;
 	std::string dataFile = constants::outputFile;
 	dataFile.insert(dataFile.find("."), std::to_string(i));
-	while (std::filesystem::exists(dataFile)) {
+	while (std::filesystem::exists(dataFile))
+	{
 		i++;
 		dataFile = constants::outputFile;
 		dataFile.insert(dataFile.find("."), std::to_string(i));
@@ -261,7 +296,8 @@ void Simulation::run()
 	// Wrap up
 	file.close();
 	auto msInt = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
-	std::cout << "\nDuration: " << (float)msInt.count() / 1000 << "seconds" << std::endl;
+	std::cout << "\nDuration: " << (float)msInt.count() / 1000 << "seconds\n"
+			  << std::endl;
 }
 
 void Simulation::normalize()
@@ -296,6 +332,5 @@ void Simulation::writeConstants(std::ofstream &f)
 	f.write((char *)&constants::anisotropy[0], 3 * sizeof(double));
 	f.write((char *)&constants::temperature, sizeof(double));
 	f.write((char *)&constants::length, sizeof(double));
+	f.write((char *)&constants::anisotropyStrength, sizeof(double));
 }
-
-
