57c5dc04846c06264ac364eb6325cdf617a8266b
timooo0@hotmail.com <36dAF5VchEsS>
2021-11-18 13:24:32 +0100

    Added theoretical plot for dispersion Fourier transform now real to complex

--- Plotting/helper.py
+++ Plotting/helper.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pyfftw
 import cmath
+import matplotlib.pyplot as plt
 
 
 constants = {
@@ -13,7 +14,15 @@ constants = {
 	
     "J_to_meV" : 6.2415093433e21,
     "Hz_to_meV": 4.135667696e-12,
+
+    "colors" : ['b','g','r','c','m','y','b'],
+
+    "pathFourier" : os.getcwd() + "\\CLaSSiC2.0\\data\\fourier.dat",
+    "pathEnergyIndex" : os.getcwd() + "\\CLaSSiC2.0\\data\\energyIndex.dat"
 }
+
+def getPath(path, i):
+    return path[:-4] + str(i) + path[-4:]
 
 def getData():
     param = []
@@ -21,6 +30,7 @@ def getData():
     basePath = os.getcwd()+"\\CLaSSiC2.0\\data\\data.dat"
     i = 0
     path = basePath[:-4] + str(i) + basePath[-4:]
+    print(path)
     while (os.path.exists(path)):
         data = np.fromfile(path, dtype=np.double)
 
@@ -34,16 +44,19 @@ def getData():
             "magneticField" : data[6:9],
             "anisotropy" : data[9:12],
             "temperature" : data[12],
-            "length" : int(data[13]) 
+            "length" : int(data[13]), 
+            "anisotropyStrength" : data[14]
         }
         param.append(parameters)
-
         x.append(np.reshape(data[param[i]["offset"]+0::3], (param[i]["steps"], param[i]["atoms"])).T)
         y.append(np.reshape(data[param[i]["offset"]+1::3], (param[i]["steps"], param[i]["atoms"])).T)
         z.append(np.reshape(data[param[i]["offset"]+2::3], (param[i]["steps"], param[i]["atoms"])).T)
 
         i += 1
+        print(x[-1].shape)
         path = basePath[:-4] + str(i) + basePath[-4:]
+    if i==0:
+        print("Could not open the file!")
     
     x = np.stack(x, axis=0)
     y = np.stack(y, axis=0)
@@ -52,44 +65,46 @@ def getData():
     return param, x, y, z
 
 def scatter(q, position, spin, param):
-    input = pyfftw.empty_aligned((param["atoms"], param["steps"]), dtype='complex128')
-    output = pyfftw.empty_aligned((param["atoms"], param["steps"]), dtype='complex128')
+    input = pyfftw.empty_aligned((param["atoms"], param["steps"]), dtype='float64')
+    output = pyfftw.empty_aligned((param["atoms"], int(param["steps"]//2+1)), dtype='complex128')
     ic_sum = 0
     for i in range(param["atoms"]):
-        q_dot_lattice = np.exp(1j * np.dot(q, position[i]))
-
-        input[i, :] = q_dot_lattice * spin[i, :]
-
-        ic_sum += cmath.exp(-1j * np.dot(q, position[i])) * spin[i, 0]
-
+        input[i, :] = spin[i, :]
+        ic_sum += np.exp(-1j * np.dot(q, position[i])) * spin[i, 0]
 
     output = pyfftw.FFTW(input, output, axes=(1,), flags=('FFTW_MEASURE',), threads=8)()
+    for i in range(param["atoms"]):
+        # if i>30: print(np.sum(np.exp(1j * np.dot(q, position[i]))))
+        output[i,:] *= np.exp(1j * np.dot(q, position[i]))
+        # plt.plot(output[i,:])
     output = np.sum(output, axis=0)
+    # for i in range(param["atoms"]):
+    #     output[i] *= np.exp(-1j * np.dot(q, position[i])) * spin[i, 0]
     output *= ic_sum
-    output = np.power(np.abs(output), 2)
-    return output[:int(param["steps"]/2)].real
+    output = np.abs(output)
+    return output
 
 def runTransform(latticePosition, spin, q, size, maxEnergyIndex, param):
     I_total = np.zeros((q.shape[0], maxEnergyIndex))
-
-    
-    eIndex = np.array([], dtype=np.float32)
     for i in range(q.shape[0]):
-        if i%size==0:
-            print(f'progress: {i/q.shape[0]*100:.3f}%')
+        print(i)
         I_aa = scatter(q[i, :], latticePosition, spin, param)
-        eIndex = np.append(eIndex, np.argmax(I_aa))
         I_total[i, :] = I_aa[:maxEnergyIndex]
 
-    for i in range(maxEnergyIndex):
-        I_total[:,i] = I_total[:,i]/np.max(I_total[:,i])
+    # for i in range(q.shape[0]):
+    #     I_total[i, :] = I_total[i, :]/np.max(I_total[i, :])
+    print(I_aa.shape, maxEnergyIndex)
+    return I_total
 
-    I_total.tofile(os.getcwd() + "\\CLaSSiC2.0\\data\\fourier.dat")
-    eIndex.tofile(os.getcwd() + "\\CLaSSiC2.0\\data\\energyIndex.dat")
+def positionLine(size, param):
+    latticePosition = []
+    for i in range(size):
+        latticePosition.append([i, 0, 0])
+    latticePosition = np.array(latticePosition).reshape(param["atoms"], 3)
 
-    return I_total, eIndex.astype(np.int32)
+    return latticePosition
 
-def positionSquareLattice(size, param):
+def positionSquare(size, param):
     latticePosition = []
     for i in range(size):
         for j in range(size):
@@ -98,7 +113,23 @@ def positionSquareLattice(size, param):
 
     return latticePosition
 
-def qAll(size):
+def scatterLine(size):
+    q = np.array([0, 0, 0])
+    q = reciprocalPath([-np.pi, 0, 0], [0, 0, 0], q, size)
+    q = reciprocalPath([0, 0, 0], [np.pi, 0, 0], q, size)
+    q = np.delete(q, 0, axis=0)
+    return q
+
+def scatterSquare(size):
+    q = np.array([0, 0, 0])
+    q = reciprocalPath([0,0,0], [np.pi, 0, 0], q, size)
+    q = reciprocalPath([np.pi,0,0], [np.pi, np.pi, 0], q, size)
+    q = reciprocalPath([np.pi,np.pi,0], [0, 0, 0], q, size)
+    q = np.delete(q, 0, axis=0)
+
+    return q
+
+def scatterAll(size):
     q = np.array([0, 0, 0])
     for i in range(size):
         for j in range(size):
@@ -108,17 +139,38 @@ def qAll(size):
 
     return q
 
-def qSquare(size):
-    q = np.array([0, 0, 0])
-    q = reciprocalPath([0,0,0], [np.pi, 0, 0], q, size)
-    q = reciprocalPath([np.pi,0,0], [np.pi, np.pi, 0], q, size)
-    q = reciprocalPath([np.pi,np.pi,0], [0, 0, 0], q, size)
-    q = np.delete(q, 0, axis=0)
-
-    return q
-
 def reciprocalPath(start, end, q, size):
     length = np.array(end) - np.array(start)
     for i in range(size): 
         q = np.vstack((q, np.array([start[0] + length[0]* i / size, start[1] + length[1]* i / size, start[2] + length[2]* i / size])))
     return q
+
+def plotMarker(structure, length, ax):
+    length = int(length/2)
+    if structure == "line":
+        ax.axvline(length)
+        ax.set_xticks([0, length, 2*length])
+        ax.set_xticklabels([r'$R$',r'$\Gamma$',r'$R$'])
+    elif structure == "square":
+        for i in range(3-1):
+            ax.axvline((1+i)*length)
+        ax.set_xticks([0, length, 2*length, 3*length])
+        ax.set_xticklabels([r'$\Gamma$',r'$M$',r'$R$',r'$\Gamma$'])
+
+def plotTheory(structure, length, param, ax, c):
+
+    if structure=="line":
+        q = scatterLine(int(length/2))
+        xData = np.linspace(0,int((q.shape[0]))-1, q.shape[0])
+        if param["J"] > 0:
+            yData = np.abs(constants["J_to_meV"]*(4*param["J"]*3.5*(1-np.cos(q[:,0]))-constants["gFactor"]*constants["bohrMagneton"]*(param["magneticField"][-1]-param["anisotropyStrength"])))
+        else:
+            a1 = (4*param["J"]*3.5)**2*(1-np.cos(q[:,0])**2)
+            a2 = 8*param["J"]*3.5**2*constants["gFactor"]*constants["bohrMagneton"]*param["anisotropyStrength"]/(2)
+            a3 = 3.5**2*(constants["gFactor"]*constants["bohrMagneton"]*param["anisotropyStrength"]/2)**2
+            b = a1+a3
+            # yData = constants["J_to_meV"]*(np.sqrt((4*param["J"]*3.5)**2*(1-np.cos(q[:,0])**2)+(constants["gFactor"]*constants["bohrMagneton"]*param["anisotropyStrength"])**2)-constants["gFactor"]*constants["bohrMagneton"]*param["magneticField"][-1])
+            yData = constants["J_to_meV"]*(np.sqrt(a1 + a2 + a3)-constants["gFactor"]*constants["bohrMagneton"]*param["magneticField"][-1])
+            ax.plot(xData, yData, constants["colors"][c], label=f'B = {param["magneticField"][-1]:.0f} T, Anis = {param["anisotropyStrength"]} T', zorder=0)
+            yData = constants["J_to_meV"]*(np.sqrt(a1 + a2 + a3)+constants["gFactor"]*constants["bohrMagneton"]*param["magneticField"][-1])
+        ax.plot(xData, yData, constants["colors"][c], label=f'B = {param["magneticField"][-1]:.0f} T, Anis = {param["anisotropyStrength"]} T', zorder=0)
