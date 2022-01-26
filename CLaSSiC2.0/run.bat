39c981431bbf60337ca7fdfb0bf00d29bbf96ea7
timooo0@hotmail.com <36dAF5VchEsS>
2022-01-26 17:07:04 +0100

    Checks if the data directory exists

--- CLaSSiC2.0/run.bat
+++ CLaSSiC2.0/run.bat
@@ -1,8 +1,11 @@
 ::No spaces!!
 @echo off
-cd data
-del *.dat
-cd ..
+if exist data\ (
+    cd data
+    del *.dat
+    del *.csv
+    cd ..
+)
 
 setlocal
 set dt=1e-15
@@ -13,15 +16,17 @@ set B=0
 set anisotropyAxis=0
 set anisotropyPlane=0
 set T=1
-@REM 0: Z-direction, 1: angle with z-axis, 2: small angle with z-axis, 3: rotor, 4: Z-direction alternating
+REM 0: Z-direction, 1: angle with z-axis, 2: small angle with z-axis, 3: rotor, 4: Z-direction alternating
 set init=2
 set angle=45
-set mode=-3
+set mode=2
+REM single, line, square, triangle, kagome
 set structure=square
-set nCellsX=30
+REM always have even number of cells
+set nCellsX=2 
 
 for %%i in (10) do ^
-model.exe -dt %dt% -steps %steps% -J %J% -lambda %lambda% -B %%i ^
+model.exe -dt %dt% -steps %steps% -J %J% -lambda %lambda% -B %B% ^
 -anisotropyAxis %anisotropyAxis% -anisotropyPlane %anisotropyPlane% ^
--T %T% -init %init% -angle %angle% -mode %mode% -structure %structure% -nCellsX %nCellsX%
+-T %T% -init %init% -angle %angle% -mode %mode% -nCellsX %nCellsX% -structure %structure%
 endlocal
