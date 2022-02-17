8aee2a4f6ebde9d8fa4610aa3b97ac3c4b15ec3c
timooo0@hotmail.com <36dAF5VchEsS>
2022-02-17 09:00:18 +0100

    bugfix kagome peak analysis

--- Plotting/ScatteringPath.py
+++ Plotting/ScatteringPath.py
@@ -13,9 +13,12 @@ energies = 4.1357e-12 * frequencies
 maxEnergyIndex = int(param[fNum]["steps"]//2+1)
 fourierLength = int(param[fNum]["steps"] / 2)
 
-print(f'z sum: {np.mean(np.sum(z[fNum],axis=0))}')
-if os.path.exists(os.getcwd() + "\\CLaSSiC2.0\\data\\fourier0.dat"):
-    os.remove(os.getcwd() + "\\CLaSSiC2.0\\data\\fourier0.dat")
+# pathFourier = helper.getPath(helper.constants["pathFourier"], fNum)
+# while os.path.exists(pathFourier):
+#     os.remove(pathFourier)
+#     fNum += 1
+#     pathFourier = helper.getPath(helper.constants["pathFourier"], fNum)
+# fNum = 0
 
 def getTransform():
     sideLength = param[fNum]["nUnitCells"]
@@ -34,7 +37,7 @@ def getTransform():
     
     elif param[fNum]["geometry"] == "kagome":
         scatterLength = int(sideLength/2) if param[fNum]["J"] >= 0 else int(sideLength/2)
-        qScatter = helper.scatterTriangle(scatterLength)
+        qScatter = helper.scatterKagome(scatterLength)
 
     nScatter = qScatter.shape[0]
     print(f'nScatter: {qScatter.shape[0]}')
@@ -47,8 +50,9 @@ def getTransform():
         I_total_x = helper.runTransform(latticePosition, x[fNum,:,:], qScatter, sideLength, maxEnergyIndex, param[fNum])
         print("y: ")
         I_total_y = helper.runTransform(latticePosition, y[fNum,:,:], qScatter, sideLength, maxEnergyIndex, param[fNum])
+        print("z: ")
         # I_total_z = helper.runTransform(latticePosition, z[fNum,:,:], qScatter, sideLength, maxEnergyIndex, param[fNum])
-        I_total = I_total_x**2 + I_total_y**2 #+ I_total_z**2
+        I_total = I_total_x**2 + I_total_y**2# + I_total_z**2
         I_total.tofile(pathFourier)
         I_total = I_total.reshape((nScatter, maxEnergyIndex))
         print(f'duration: {time.time()-start}')
@@ -60,30 +64,54 @@ def getTransform():
     return I_total, nScatter, sideLength
 
 fig, ax = plt.subplots(2)
-point = 2
+point = 24
+usePeaks = False
 while fNum < x.shape[0]:
     I_total, nScatter, sideLength = getTransform()
-    xData = np.linspace(0,nScatter-1,nScatter)
-    yData, zData = [], []
+    # xData = np.linspace(0,nScatter-1,nScatter)
+    xData, yData, zData = [], [], []
     maxF = np.log10(np.max(I_total))
-    for j in range(nScatter):
-        yData.append(np.abs(energies[np.argmax(I_total[j,:])]))
-        zData.append(max([np.log10(np.max(I_total[j,:]))-maxF, -5]))
+    for i in range(nScatter):
+        if usePeaks:
+            peaks, _ = find_peaks(I_total[i, :], height=np.max(I_total[i, :])/1000, distance=50)
+            # Find peaks cannot check if the peaks at index 0
+            if I_total[i, 0] >= np.max(I_total[i, :])/10:
+                yData.append(np.abs(energies[0]))
+                xData.append(i)
+                zData.append(max([np.log10(np.max(I_total[i,0]))-maxF, -5]))
 
-    # ax[0].plot(xData,yData, helper.constants["colors"][fNum+1]+'o')
-    # ax[0].plot(xData,yData3, helper.constants["colors"][fNum]+'o')
-    print(xData.dtype, type(yData[0]), type(zData))
+            # If there are no peaks take the highest value
+            if peaks.size == 0:
+                yData.append(np.abs(energies[np.argmax(I_total[i,:])]))
+                xData.append(i)
+                zData.append(max([np.log10(np.max(I_total[i,:]))-maxF, -5]))
+
+            # Plot all the peaks
+            else:
+                for peak in peaks:
+                    yData.append(np.abs(energies[peak]))
+                    xData.append(i)
+                    zData.append(max([np.log10(np.max(I_total[i, peak]))-maxF, -5]))
+        
+        else:
+            yData.append(np.abs(energies[np.argmax(I_total[i,:])]))
+            xData.append(i)
+            zData.append(max([np.log10(np.max(I_total[i,:]))-maxF, -5]))
+
+    # ax[0].plot(xData,yData, helper.constants["colors"][fNum]+'o')
     helper.plotTheory(param[fNum]["geometry"], sideLength, param[fNum], ax[0], fNum)
     im = ax[0].scatter(xData,yData,c=zData,cmap="Wistia", zorder=10)
-    fig.colorbar(im, ax=ax[0])
+    # fig.colorbar(im, ax=ax[0])
 
+    helper.plotPeaks(ax[1], energies[:maxEnergyIndex-1], I_total[point,:])
+    ax[1].plot(energies[:maxEnergyIndex-1], I_total[point,:maxEnergyIndex-1], c= helper.constants["colors"][fNum])
+    helper.plotSmooth(ax[1], energies[:maxEnergyIndex-1], I_total[point,:])
     fNum = fNum + 1
 
 fNum = fNum - 1
 
-ax[1].plot(energies[:maxEnergyIndex-1], I_total[point,:maxEnergyIndex-1])
-# ax[1].set_yscale("log")
-ax[0].axvline(xData[point], c='r')
+ax[1].set_yscale("log")
+ax[0].axvline(point, c='r')
 ax[0].set_ylabel('Energy [meV]')
 ax[0].legend(loc="lower right")
 
