7bc84f5c64190978d570ae3023129935ce9e9b5d
timooo0@hotmail.com <36dAF5VchEsS>
2022-03-01 14:59:55 +0100

    Fix twice loop variable

--- CLaSSiC2.0/run.bat
+++ CLaSSiC2.0/run.bat
@@ -10,24 +10,24 @@ if exist data\ (
 setlocal
 set dt=1e-15
 set steps=1e6
-set J=2
+set J=-2
 set lambda=1e-3
 set B=0
 set anisotropyAxis=0
 set anisotropyPlane=0
-set T=0
+set T=1e-1
 REM 0: Z-direction, 1: angle with z-axis, 2: small angle with z-axis, 3: rotor, 4: Z-direction alternating
-set init=3
-set angle=30
-set mode=2
+set init=7
+set angle=20
+set mode=0
 REM single, line, square, triangle, kagome
-set structure=line
+set structure=kagome
 REM always have even number of cells
-set nCellsX=2 
+set nCellsX=6 
 set periodicBoundary=true
 
-for %%i in (0,10,50,70) do ^
+for %%i in (0) do ^
 model.exe -dt %dt% -steps %steps% -J %J% -lambda %lambda% -B %B% ^
 -anisotropyAxis %anisotropyAxis% -anisotropyPlane %anisotropyPlane% ^
--T %%i -init %init% -angle %%i -mode %mode% -nCellsX %nCellsX% -periodicBoundary %periodicBoundary% -structure %structure%
+-T %T% -init %init% -angle %angle% -mode %mode% -nCellsX %nCellsX% -periodicBoundary %periodicBoundary% -structure %structure%
 endlocal
