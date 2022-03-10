0c8b437dfc5cf6d45587d713478948e918a8814f
timooo0@hotmail.com <36dAF5VchEsS>
2022-03-10 10:54:14 +0100

    Added axis labels

--- Plotting/energy.py
+++ Plotting/energy.py
@@ -1,6 +1,7 @@
 import helper
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 param, x, y, z = helper.getData()
 fNum = 0
@@ -9,8 +10,12 @@ fig, ax = plt.subplots()
 end = int(1e4)
 time = np.linspace(0,param[fNum]["dt"]*param[fNum]["steps"],param[fNum]["steps"])
 energy = helper.getEnergy(fNum)
-ax.plot(time[:end], energy[:end])
+ax.plot(time[1:end], energy[1:end]/param[fNum]["atoms"])
+# ax.yaxis.set_major_formatter(FormatStrFormatter('%.5f'))
+print(np.isnan(np.sum(energy)))
+print(energy[0])
 # ax.set_yscale("log")
-
+ax.set_xlabel("time [s]")
+ax.set_ylabel("energy/N [meV]")
 
 plt.show()
